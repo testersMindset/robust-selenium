6dbb7c5b98df3848eaacf7a83af565e6c770f5dd
testersMindset <rajuptester@gmail.com>
2020-01-02 22:37:00 -0600

    Updated chromedriver and cleanup..

--- src/main/java/com/robust/selenium/configs/Browser.java
+++ src/main/java/com/robust/selenium/configs/Browser.java
@@ -2,14 +2,14 @@ package com.robust.selenium.configs;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.robust.common.utils.DataStore;
-import com.robust.common.utils.JSONFileReader;
+import com.robust.common.utils.PropertiesReader;
 
 public class Browser {
 
@@ -18,40 +18,22 @@ public class Browser {
 
 	public static WebDriver driver() {
 		log.info("launching browser .. ");
-		String launchMode = System.getProperty("browser.launchmode");
+		String launchMode = PropertiesReader.getValue("browser.launchmode");
 		if (webDriver == null) {
 			System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 			webDriver = "headless".equalsIgnoreCase(launchMode) ? new PhantomJSDriver(getDesiredCapabilities())
-					: new ChromeDriver();
+					: new ChromeDriver(getChromeOptions());
 		}
 		return webDriver;
 	}
 
-	/**
-	 * Above same conditions can be provided thru json file as well. 
-	 * Example: Read data from json file to run test. generates driver from given values in json file
-	 * 
-	 * this is just example, launching browser mode by hardcoding into json file is not 
-	 * considered as best practice. This can be passed thru runconfig & parameterizing in jenkins,
-	 * if thats' how test is going to run
-	 */
-	/*
-	 * public static WebDriver driverThruJsonFile() {
-	 * log.info("launching browser .. "); 
-	 * JSONFileReader.getJSONConfigData();
-	 * String launchMode = DataStore.getJsonObject().getString("browserMode"); 
-	 * if (webDriver == null) 
-	 * 		{ System.setProperty("webdriver.chrome.driver", ./Drivers/chromedriver.exe"); 
-	 * 		webDriver = "headless".equalsIgnoreCase(launchMode) 
-	 * 					? new PhantomJSDriver(getDesiredCapabilities()) 
-	 * 					: new ChromeDriver(); 
-	 * 		}
-	 * 
-	 * 	return webDriver; 
-	 * }
-	 * 
-	 */
-
+	
+	private static ChromeOptions getChromeOptions() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--incognito");
+		return options;
+	}
+	
 	public static void setDriverNull() {
 		webDriver = null;
 	}
